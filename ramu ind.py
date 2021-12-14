d52426733486526f65422544d3af1ce2d9f2896e
sivarebel-18801 <95667428+sivarebel-18801@users.noreply.github.com>
2021-12-14 07:57:56 +0530

    Add files via upload

---
+++ ramu ind.py
@@ -0,0 +1,28 @@
+a='computer_science'
+print(a[0])
+print(a[-1])
+print(a[4],a[-4],a[3],a[-3],a[2],a[-2],a[7],a[15],a[-15])
+print(a[:4])
+print('u' in a)
+print(a[::-4])
+x=10
+y='python'
+x=input()
+print(x)
+print(type(x))
+a=int(input('enter one inter:' ))
+print(a)
+print(type(a))
+a=float(input('enter one float:' ))
+print(a)
+print(type(a))
+a=bool(input('enter one bool:' ))
+print(a)
+print(type(a))
+rad=2.0
+pi=3.14
+area=pi*rad*rad
+print(area)
+a=int(input('area:'))
+print(type(a))
+
