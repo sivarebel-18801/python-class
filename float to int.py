cda090ca78c5b81c68b4edf0a674bbb0c21fd517
sivarebel-18801 <95667428+sivarebel-18801@users.noreply.github.com>
2021-12-15 08:17:46 +0530

    Add files via upload

---
+++ float to int.py
@@ -0,0 +1,26 @@
+r=2.1
+pi=3.14
+area=pi*r*r
+print(area)
+print(type(int(area)))
+a='area of cone'
+a=pi*r*r*3.0
+pi=3.14
+r=2.1
+print(a)
+print(type(int(a)))
+vad='google_chrome'
+print(vad[2:10])
+print(vad[2:10:1])
+print(vad[5:1])
+print(vad[:5:1])
+print(vad[:-5:1])
+print(vad[8:2])
+print(vad[2:10:2])
+print(vad[2:10:3])
+print(vad[-10:10])
+print(vad[-10:10:1])
+print(vad[-10:10:2])
+print(vad[-10:10:3])
+
+      
