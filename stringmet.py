22e12ff498388abda9c15af6738f2961b7c57d5a
sivarebel-18801 <95667428+sivarebel-18801@users.noreply.github.com>
2021-12-18 21:34:56 +0530

    Add files via upload

---
+++ stringmet.py
@@ -0,0 +1,13 @@
+a='narasimha'
+print(a[4])
+vad='super'
+print(vad[0],len(vad),vad[-1],sep='')
+a='siva'
+b='ramu'
+print(a[0],b[1:],b[0],a[1:],len(a),len(b),a[2],b[2],sep='')
+a='      siva'
+b='rmau   '
+c='  king  '
+print(a.lstrip())
+print(b.rstrip())
+print(c.strip())
