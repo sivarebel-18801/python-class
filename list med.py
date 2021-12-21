7ac501b190188cb147d694e11f061d1e95acab37
sivarebel-18801 <95667428+sivarebel-18801@users.noreply.github.com>
2021-12-21 08:24:38 +0530

    Add files via upload

---
+++ list med.py
@@ -0,0 +1,38 @@
+string=input()
+print(string)
+middle_index=len(string)//2
+print(middle_index)
+
+a=input('please enter a=')
+b=input('please enter b=')
+c=input('please enter c=')
+print(a)
+print(b)
+print(c)
+
+a='siva'
+b='basha'
+char='i'
+print(char)
+
+x=[]
+print(x)
+print(type(x))
+a=[1,2,3]
+d=[4,5,6]
+print(a+d)
+
+a=[8,9,1,5,6,7,8]
+print(sum(a))
+print(min(a))
+print(max(a))
+
+a=[1,2,3,3,3,4,5,5,5]
+print(a.count(3))
+print(a.remove(5))
+print(type(tuple(a)))
+
+
+
+
+
