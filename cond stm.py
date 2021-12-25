410cf2485f6467ae8b9ffac050a2711df3fd6cb8
sivarebel-18801 <95667428+sivarebel-18801@users.noreply.github.com>
2021-12-25 14:53:12 +0530

    Add files via upload

---
+++ cond stm.py
@@ -0,0 +1,22 @@
+a=int(input("enter your marks,it should be less than 100and greater than 0:"))
+if a>90:
+    print('grade A')
+elif a>80:
+    print('grade B')
+elif a>70:
+    print('grade C')
+elif a>60:
+    print('grade D')       
+else:
+    print('fail')
+
+a=100
+if a==90:
+    print("A+")
+elif a>78:
+    print('B+')
+else:
+    print('fail')
+
+    
+
