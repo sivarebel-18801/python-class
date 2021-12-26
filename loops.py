a955f2d64083804801175f97fc8a823e17d1ccf7
sivarebel-18801 <95667428+sivarebel-18801@users.noreply.github.com>
2021-12-26 08:26:28 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,52 @@
+print(list(range(0,75,5)))
+print(list(range(0,72,8)))
+print(list(range(75,0,-5)))
+print(list(range(96,72,-8)))
+print(list(range(5)))
+print(list(range(10,0,-2)))
+print(list(range(10,100,5)))
+print(list(range(-5,5,3)))
+print(list(range(10,0,-1)))
+print(list(range(10,100,-10)))
+print(list(range(-5,5)))
+print(list(range(5,20)))
+print(list(range(1,5)))
+
+a=[10,100]
+for i in range(10,100):
+    if i*8==0:
+       print(i)
+
+
+a=[1,2,3,4,5,6,7]
+print(a)
+for i in a:
+    print(i)
+a.append(8)
+print(a)
+
+a={1:89,2:76,3:56,4:46,5:36}
+print(a)
+for i in a:
+    print(i)
+    print("keys",a)
+    print("values",a)
+
+
+x=[1,2,3,4,5,6,7,8,910]
+for i in range(10):
+    if i%2==0:
+        print('even',i)
+    else:
+        print('odd',i)
+
+
+
+
+
+
+
+        
+
+
+
